b4ff38e4e067d0d1c52854d4c772cbcdef5f7a7e
Nitin Tiwari <128287787+thisisnitintiwarii@users.noreply.github.com>
2026-01-31 13:31:55 +0530

    Change SameSite cookie attribute to 'none'

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const signup = async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
     });
 
     return res.status(201).json({
@@ -85,7 +85,7 @@ export const login = async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
     });
 
     return res.status(200).json({
